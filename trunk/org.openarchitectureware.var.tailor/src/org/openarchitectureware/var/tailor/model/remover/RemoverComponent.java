2c71006bc4cfee5419536639d40a3f52012d06b0
andy.rytina <andy.rytina@ac2ce7f4-c901-11dd-a2c4-6dc3ad32ce87>
2009-02-23 09:14:35 +0000

    throw WorkflowInterruptedException, if no configurationWrapper is set

--- trunk/org.openarchitectureware.var.tailor/src/org/openarchitectureware/var/tailor/model/remover/RemoverComponent.java
+++ trunk/org.openarchitectureware.var.tailor/src/org/openarchitectureware/var/tailor/model/remover/RemoverComponent.java
@@ -2,17 +2,20 @@ package org.openarchitectureware.var.tailor.model.remover;
 
 
 import org.eclipse.emf.ecore.EObject;
+import org.openarchitectureware.util.stdlib.DynamicEcoreHelper;
 import org.openarchitectureware.var.featureaccess.ConfigurationModelWrapper;
 import org.openarchitectureware.var.featureaccess.FeatureDependentComponent;
 import org.openarchitectureware.var.featureaccess.FeatureModelWrapper;
 import org.openarchitectureware.var.featureaccess.ext.FeatureSupport;
 import org.openarchitectureware.workflow.WorkflowContext;
+import org.openarchitectureware.workflow.WorkflowInterruptedException;
 import org.openarchitectureware.workflow.issues.Issues;
 import org.openarchitectureware.workflow.monitor.ProgressMonitor;
 
 public class RemoverComponent extends FeatureDependentComponent {
 	
 	private String architectureModelSlot;
+	
 
 	public void setArchitectureModelSlot( String slotname ) {
 		architectureModelSlot = slotname;
@@ -36,11 +39,13 @@ public class RemoverComponent extends FeatureDependentComponent {
 			issues.addError("no contents found in slot "+architectureModelSlot);
 			return;
 		}
+		
 		ConfigurationModelWrapper configurationWrapper = getConfigurationModel(ctx, issues);
+		if ( configurationWrapper == null ) throw new WorkflowInterruptedException("no configurationWrapper defined!");
 		FeatureSupport.setConfigurationModelWrapper(configurationWrapper);
-		if ( configurationWrapper == null )  {
-			return;
-		}
+		DynamicEcoreHelper h = new DynamicEcoreHelper(architectureModel.eClass().getEPackage());
+		FeatureSupport.setDynamicEcoreHelper(h);
+
 		Remover r = new Remover();
 		r.remove( architectureModel, configurationWrapper );
 	}
