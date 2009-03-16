33603eaeccea68e3e26424600d7e8ea7ae76990e
andy.rytina <andy.rytina@ac2ce7f4-c901-11dd-a2c4-6dc3ad32ce87>
2009-03-16 22:53:40 +0000

    -rename src -> model -make some changes to model

---
+++ demo/trunk/test/src-variants/com/demo/logic/vOIPConnection/VOIPConnection.java
@@ -0,0 +1,14 @@
+package com.demo.logic.vOIPConnection;
+
+import com.demo.logic.IAudioConnectionRequest;
+import com.demo.logic.IAudioConnectionResponse;
+
+public class VOIPConnection extends VOIPConnectionBase {
+
+	@Override
+	public IAudioConnectionResponse requestCall(IAudioConnectionRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
