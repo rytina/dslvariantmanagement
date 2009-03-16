5f19cc50a1ae81cc2ffb59b623472eb4f5ed5ede
andy.rytina <andy.rytina@ac2ce7f4-c901-11dd-a2c4-6dc3ad32ce87>
2009-03-16 22:53:40 +0000

    -rename src -> model -make some changes to model

---
+++ trunk/demo/trunk/test/src-variants/com/demo/logic/instantMessagingService/InstantMessagingService.java.v
@@ -0,0 +1,74 @@
+package com.demo.logic.instantMessagingService;
+
+import  java.lang.Math;
+
+import com.demo.logic.IAudioServiceRequest;
+import com.demo.logic.IAudioServiceResponse;
+import com.demo.logic.IInstantMessagingServiceRequest;
+import com.demo.logic.IMessageDispatcherRequest;
+import com.demo.logic.IMessageDispatcherResponse;
+
+public class InstantMessagingService extends InstantMessagingServiceBase {
+
+	@Override
+	public IMessageDispatcherResponse requestDispatch(
+			IMessageDispatcherRequest request) {
+		if( request.getConversationID() == 0 ){
+			request.setConversationID( (int) System.currentTimeMillis() );
+		}
+		
+		//convert generic message request to instant message request
+		IInstantMessagingServiceRequest msg = new IInstantMessagingServiceRequest();
+		msg.setMessage( request.getMessage() );
+		msg.setReceiverID( request.getSourceID() );
+		msg.setSenderID( request.getDestinationID() );
+		
+		//delegate message to the sender-method for the instant messenger
+		receiveSendMessage(msg);
+		
+		IMessageDispatcherResponse response = new IMessageDispatcherResponse();
+		response.setConversationID(request.getConversationID());
+		return response;
+	}
+
+	@Override
+	public void receiveSendMessage(IInstantMessagingServiceRequest message) {
+		//send message
+	}
+
+	@Override
+	public IAudioServiceResponse requestDial(IAudioServiceRequest request) {
+		
+		//#TelephoneIntegration
+		IPAudioConRequirer audioCon = audioCon();
+		//make TelephoneIntegration stuff
+		return null;
+		//~
+		
+		//#VoiceOverIP
+		IPAudioConRequirer audioCon = audioCon();
+		//make VoiceOverIP stuff
+		return null;
+		//~
+		
+	}
+	
+	@Override
+	public void receiveAddToConference(IAudioServiceRequest message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveHangUp(IAudioServiceRequest message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveMicOff(IAudioServiceRequest message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
