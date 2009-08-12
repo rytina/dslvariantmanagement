81406707ed6efea78f0d9ea56c1ebbb0ffb3dedd
andy.rytina <andy.rytina@ac2ce7f4-c901-11dd-a2c4-6dc3ad32ce87>
2009-08-12 06:23:18 +0000

    rename projects/packages to org.openarchitectureware.*

--- trunk/oAW5/trunk/org.eclipse.xtext.var.actions/src/org/openarchitectureware/var/actions/properties/PropertyConstants.java
+++
@@ -1,7 +0,0 @@
-package org.openarchitectureware.var.actions.properties;
-
-public interface PropertyConstants {
-
-	public static final String ENABLE_PROJECT_SETTINGS_PROP = "DOTV.EnableProjectSpecificSettings";
-	
-}
--- trunk/oAW5/trunk/test/dot/processdot.bat
+++
@@ -1,9 +0,0 @@
-
-		@echo off
-	    mkdir .
-		echo Using Graphviz in path "%GRAPHVIZ_BIN%/" 
-		
-			echo processing examplevis.dot ...
-			"%GRAPHVIZ_BIN%/dot.exe" -Tgif examplevis.dot -o ./examplevis.gif
-		
-	
