07d13b48cbf3108db18d39d768832fbdd2dd48ce
andy.rytina <andy.rytina@ac2ce7f4-c901-11dd-a2c4-6dc3ad32ce87>
2009-11-13 09:39:14 +0000

    when opening the DotVBuilder properties page in the Eclipse Workbench (not the Runtime Workbench) an NPE occurred, because the DotvBuilder was not loaded.  I solved the problem by loading the DotVBuilder-Plugin on eclipse startup.

--- trunk/oAW5/trunk/org.openarchitectureware.var.dotvbuilder/src/org/openarchitectureware/var/dotvbuilder/properties/DotVPropertyPage.java
+++ trunk/oAW5/trunk/org.openarchitectureware.var.dotvbuilder/src/org/openarchitectureware/var/dotvbuilder/properties/DotVPropertyPage.java
@@ -3,7 +3,9 @@ package org.openarchitectureware.var.dotvbuilder.properties;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -14,6 +16,7 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.PropertyPage;
 import org.openarchitectureware.var.dotvbuilder.Activator;
