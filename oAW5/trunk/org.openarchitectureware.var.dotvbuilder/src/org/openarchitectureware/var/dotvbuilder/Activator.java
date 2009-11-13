c12cc196ac14bf48de9f8eda35be2c63f169556f
andy.rytina <andy.rytina@ac2ce7f4-c901-11dd-a2c4-6dc3ad32ce87>
2009-11-13 09:39:14 +0000

    when opening the DotVBuilder properties page in the Eclipse Workbench (not the Runtime Workbench) an NPE occurred, because the DotvBuilder was not loaded.  I solved the problem by loading the DotVBuilder-Plugin on eclipse startup.

--- oAW5/trunk/org.openarchitectureware.var.dotvbuilder/src/org/openarchitectureware/var/dotvbuilder/Activator.java
+++ oAW5/trunk/org.openarchitectureware.var.dotvbuilder/src/org/openarchitectureware/var/dotvbuilder/Activator.java
@@ -4,13 +4,15 @@
  **************************************************/
 package org.openarchitectureware.var.dotvbuilder;
 
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.IStartup;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends AbstractUIPlugin implements IStartup{
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.openarchitectureware.var.dotvbuilder";
@@ -52,6 +54,10 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public static Activator getDefault() {
 		return plugin;
+	}
+
+	public void earlyStartup() {
+		Platform.getPlugin("org.openarchitectureware.var.dotvbuilder");
 	}
 
 }
